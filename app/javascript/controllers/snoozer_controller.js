e59fe56b8336ce56f442a493f1f9a8d497a3b566
DAZ <darren.jones@hey.com>
2025-05-29 17:04:09 +0100

    updated snooze ui

--- app/javascript/controllers/snoozer_controller.js
+++ app/javascript/controllers/snoozer_controller.js
@@ -1,7 +1,25 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  submit(event) {
-    this.element.requestSubmit();
+  static targets = ["menu"];
+
+  toggle() {
+    this.menuTarget.hidden = !this.menuTarget.hidden;
+  }
+
+  select(event) {
+    const snoozedUntil = event.currentTarget.dataset.value;
+    const id = this.element.dataset.taskId;
+    this.menuTarget.hidden = true;
+    fetch(`/tasks/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "text/vnd.turbo-stream.html",
+        "X-CSRF-Token": document.querySelector("meta[name='csrf-token']")
+          .content,
+      },
+      body: JSON.stringify({ task: { snoozed_until: snoozedUntil } }),
+    });
   }
 }
