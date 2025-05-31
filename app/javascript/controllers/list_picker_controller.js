80a8ac7831327ea215a77d096b76e67d6ecd8144
DAZ <darren.jones@hey.com>
2025-05-31 15:48:07 +0100

    implemented burger menu

---
+++ app/javascript/controllers/list_picker_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["lists"];
+  static values = {
+    listPicker: String,
+  };
+
+  connect() {
+    this.clickOutsideHandler = this.handleClickOutside.bind(this);
+    document.addEventListener("click", this.clickOutsideHandler);
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.clickOutsideHandler);
+  }
+
+  handleClickOutside(event) {
+    if (!this.element.contains(event.target)) {
+      this.listsTarget.hidden = true;
+    }
+  }
+
+  toggle() {
+    this.listsTarget.hidden = !this.listsTarget.hidden;
+  }
+
+  select(event) {
+    const listId = event.currentTarget.dataset.listPickerValue;
+    this.listsTarget.hidden = true;
+
+    if (listId) {
+      window.location.href = `/lists/${listId}`;
+    }
+  }
+}
--- app/javascript/controllers/snoozer_controller.js
+++ app/javascript/controllers/snoozer_controller.js
@@ -3,12 +3,27 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["menu"];
 
+  connect() {
+    this.clickOutsideHandler = this.handleClickOutside.bind(this);
+    document.addEventListener("click", this.clickOutsideHandler);
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.clickOutsideHandler);
+  }
+
+  handleClickOutside(event) {
+    if (!this.element.contains(event.target)) {
+      this.menuTarget.hidden = true;
+    }
+  }
+
   toggle() {
     this.menuTarget.hidden = !this.menuTarget.hidden;
   }
 
   select(event) {
-    const snoozedUntil = event.currentTarget.dataset.value;
+    const snoozedUntil = event.currentTarget.dataset.snoozerValue;
     const id = this.element.dataset.taskId;
     this.menuTarget.hidden = true;
     fetch(`/tasks/${id}`, {
