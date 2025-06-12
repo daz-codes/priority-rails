ea42eed89aadcc795c7bb4a75e89ddd45029624e
DAZ <darren.jones@hey.com>
2025-06-12 19:18:43 +0100

    updated completed task page

--- app/models/task.rb
+++ app/models/task.rb
@@ -17,8 +17,9 @@ class Task < ApplicationRecord
   scope :completed, -> { where.not(completed_on: nil).order(completed_on: :desc) }
   scope :completed_today, -> { where(completed_on: Date.current.all_day).ordered }
   scope :completed_yesterday, -> { where(completed_on: 1.day.ago.all_day).ordered }
-  scope :completed_this_week, -> { where(completed_on: 3.days.ago ... 7.days.ago).ordered }
-  scope :completed_before_today, -> { where('completed_on < ?', Date.current.beginning_of_day).ordered }
+  scope :completed_this_week, -> { where(completed_on: 7.days.ago ... 1.day.ago).ordered }
+  scope :completed_more_than_a_week_ago, -> { where("completed_on < ?", 7.days.ago).ordered }
+  scope :completed_before_today, -> { where("completed_on < ?", Date.current.beginning_of_day).ordered }
   scope :incomplete, -> { where(completed_on: nil).ordered }
   scope :snoozed, -> { where("snoozed_until > ?", Time.current).order(snoozed_until: :asc) }
   scope :unsnoozed, -> { where("snoozed_until IS NULL OR snoozed_until <= ?", Time.current).ordered }
